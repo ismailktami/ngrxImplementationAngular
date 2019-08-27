1324971ca1ee3f1404e78a586ee283fc2afc2229
ismailKtami <ktamiismail@gmail.com>
2019-08-27 23:46:08 +0100

    add store-devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import {reducer} from "./store/reducers/user.reducer"
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {counterReducer} from "./store/reducers/counter.reducer"
+import {StoreDevtoolsModule} from "@ngrx/store-devtools"
 @NgModule({
   declarations: [
     AppComponent
@@ -14,7 +15,10 @@ import {counterReducer} from "./store/reducers/counter.reducer"
     StoreModule.forRoot({
       user:reducer,
       counter:counterReducer
-    })
+    }),
+    StoreDevtoolsModule.instrument(
+      {}
+      )
   ],
   providers: [],
   bootstrap: [AppComponent]
